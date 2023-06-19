83989bc6557b813e5316ce7d677fd4ece985f72d
Evan <evanhill925@gmail.com>
2023-06-18 18:44:30 -0700

    yolo

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
-
 require('dotenv').config()
+const uri = process.env.DB_STRING
+const client = new MongoClient(uri);
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'Todo'
-
-
-    async function connectDB(){
-await MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-.then(client => {
-    console.log(`Connected to ${dbName} Database`)
-    db = client.db(dbName)
-})
+// make a function that awaits mongo connection before allowing requests
+async function connectToMongo() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
     }
-   
+}
+connectToMongo().catch(console.error);
+
+
+console.log("here first")
+app.get("/", async (req, res) => {
+    let my_item = req.params.my_item;
+    let item = await client.db("Todo")
+                .collection("Todos")
+                .find().toArray()
+                .then( data => {
+                 return res.render('index.ejs', { info: data})
+                })
+.catch( error => console.error(error))
+})
+
+
+// client.connect(err => {
+//     if(err){ console.error(err); return false;}
+//     // connection to mongo is successful, listen for requests
+//     app.listen(PORT, () => {
+//         console.log("listening for requests");
+//     })
+// });
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+// .then(client => {
+//     console.log(`Connected to ${dbName} Database`)
+//     db = client.db(dbName)
+// })
+console.log("here third")
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -25,28 +52,12 @@ app.use(express.json())
 
 
 
-// app.get('/', async (req, res) => {
-//     await db.collection('bills').find().toArray()
-//     .then( data => {
-//         res.render('index.ejs', { info: data})
-//     })
-//     .catch( error => console.error(error))
-// })
 
-// app.get('/',async (req, res)=>{
-//     const billItems = await db.collection('todos').find().toArray()
-    
-//    return res.render('index.ejs', { items: billItems})
-// })
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('Todos').find().toArray()
-    response.render('index.ejs', { items: todoItems})
-})
 
 
 app.post('/addBill', (req, res) => {
-    db.collection('Todos').insertOne({ billName: req.body.billName, billAmount: req.body.billAmount})
+    db.collection('bills').insertOne({ billName: req.body.billName, billAmount: req.body.billAmount})
     .then(result => {
         res.redirect('/')
     })
@@ -66,7 +77,6 @@ app.delete('/deleteBill', (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
-connectDB()
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
